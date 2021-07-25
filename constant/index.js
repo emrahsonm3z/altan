41c81909a516ec19adb664d7c6fb23bec40b9bad
emrahsonm3z <emrah.sonmez@itsbilisim.com>
2021-07-25 18:17:28 +0300

    initial commit

---
+++ constant/index.js
@@ -0,0 +1,3 @@
+export const FB_APP_ID = "1144963732653627";
+export const FB_APP_SECRET = "5b376f82edd72ad844b68a7ecd1484cb";
+export const REDIRECT_URI = "http://localhost:3000/fb";
---
+++ pages/fb.js
@@ -0,0 +1,5 @@
+function fb() {
+  return <div>facebook ile login olundu</div>;
+}
+
+export default fb;
