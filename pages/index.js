5e036fc6b088b999aefcce1853d92a0608aa6955
emrahsonm3z <emrah.sonmez@itsbilisim.com>
2021-07-25 18:37:00 +0300

    sdfsfa

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,30 @@
 import axios from "axios";
 import { FB_APP_ID, REDIRECT_URI } from "../constant";
-
-const params = { fblogin: 1 };
+let params = "fbloged=1";
 export default function Home() {
   const handleClick = () => {
     console.log("🚀 -> handleClick -> handleClick");
 
-    const response = axios
-      .get(`https://www.facebook.com/v11.0/dialog/oauth`, {
-        client_id: FB_APP_ID,
-        redirect_uri: REDIRECT_URI,
-        state: params,
-      })
+    const uriXX = `https://www.facebook.com/v11.0/dialog/oauth?
+    client_id=${FB_APP_ID}
+    &redirect_uri=${REDIRECT_URI}
+    &state=${params}`;
+
+    console.log(uriXX);
+    axios
+      .get(uriXX)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   };
+
+  // const response = axios
+  // .post(`https://www.facebook.com/v11.0/dialog/oauth`, {
+  //   client_id: FB_APP_ID,
+  //   redirect_uri: REDIRECT_URI,
+  //   state: params,
+  // })
+  // .then((res) => console.log(res))
+  // .catch((err) => console.log(err));
 
   return (
     <div>
